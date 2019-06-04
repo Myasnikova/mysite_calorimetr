0282629edb37c5d849b3638072574da15034b501
Liza <emyaska@bk.ru>
2019-06-04 08:15:20 +0300

    allow hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&^est$_$&52^nys@=%np202z&w-qow
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calorimetr.herokuapp.com','127.0.0.1']
 
 
 # Application definition
